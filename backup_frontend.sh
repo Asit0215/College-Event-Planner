df7cddf27ce54ef61c805d76124e154dae95383e
Afnan Nasyam <afnannasyam@gmail.com>
2025-06-17 15:16:20 +0530

    added automated shell script for backing-up frontend files

---
+++ backup_frontend.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# configuration
+SOURCE_DIR="/c/Users/afnan/college-event-planner/College-Event-Planner"
+BACKUP_DIR="/c/Users/afnan/frontend_backups"
+LOG_FILE="/c/Users/afnan/frontend_backup.log"
+
+# creating backup
+mkdir -p "$BACKUP_DIR"
+
+# timestamp for backup
+TIMESTAMP=$(date "+%Y-%m-%d_%H-%M")
+BACKUP_FILE="$BACKUP_DIR/frontend_backup_$TIMESTAMP.tar.gz"
+
+# backing up html files
+tar -czf "$BACKUP_FILE" -C "$SOURCE_DIR" $(find "$SOURCE_DIR" -maxdepth 1 -name "*.html" -exec basename {} \;)
+
+# shows log succes
+echo "[$(date)] Backup created: $BACKUP_FILE" >> "$LOG_FILE"
