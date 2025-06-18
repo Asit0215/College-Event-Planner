efd2b4e7acb9148e1e47e3fa9f3b71d9dfafee60
AparnaSathiya01 <aparna.s2@cognizant.com>
2025-06-18 13:52:24 +0530

    home.js

--- home page.js
+++ home page.js
@@ -9,18 +9,31 @@ $(document).ready(function () {
         {
           scrollTop: $(target).offset().top,
         },
-        600 // duration in ms
+        600
       );
     }
   });
 
-  // Hover effects
-  $(".event-card").on("mouseenter", function () {
-    $(this).addClass("ring-2 ring-blue-300");
-  }).on("mouseleave", function () {
-    $(this).removeClass("ring-2 ring-blue-300");
+  $(".event-card").hover(
+    function () {
+      $(this).addClass("ring-2 ring-blue-300");
+    },
+    function () {
+      $(this).removeClass("ring-2 ring-blue-300");
+    }
+  );
+
+  // Show username from localStorage
+  const username = localStorage.getItem("username") || "Guest";
+  $("#usernameDisplay").text("Welcome, " + username);
+
+  $("#profileBtn").click(function () {
+    $("#profileDropdown").toggle();
   });
 
-  // Set user role to localStorage for access control
-  localStorage.setItem("userRole", "student");
+  $("#logoutBtn").click(function () {
+    localStorage.clear();
+    alert("You have been logged out.");
+    window.location.href = "login.html";
+  });
 });
